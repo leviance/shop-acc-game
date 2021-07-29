cd5bf7d1a8bccf828385ec3298d07b96e3f52151
drdung1999 <duongdung12a8@gmail.com>
2021-07-29 11:48:10 +0700

    complete change for readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# web site to sell game account
+# Website to sell game account
 
 shop account game, web site sell account game, game web
 
---
+++ backend/env/example_env.sh
@@ -0,0 +1,13 @@
+export PORT=3001
+export HOST=localhost
+
+export CLIENT_URL=localhost:8080
+
+export DB_CONNECTION=mongodb
+export DB_HOST=localhost
+export DB_PORT=27017
+export DB_NAME=shopgame
+
+export SESSION_SECRET=test
+
+export NODE_ENV=development
